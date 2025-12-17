174736ddf87a45addf77201a6fb50ff3049d1980
5yrotciv <osw9921@gmail.com>
2025-12-18 08:39:51 +0900

    feat: 리소스 매니저

--- pokemon-clone/pokemon-clone/Player.cpp
+++ pokemon-clone/pokemon-clone/Player.cpp
@@ -4,8 +4,11 @@
 #include "InputManager.h"
 #include "TimeManager.h"
 #include "ObjectManager.h"
+#include "ResourceManager.h"
 
 #include "Missile.h"
+
+#include "LineMesh.h"
 
 Player::Player ( ) : Object(ObjectType::Player )
 {
@@ -61,5 +64,7 @@ void Player::Update ( )
 
 void Player::Render ( HDC hdc )
 {
-	Utils::DrawCircle ( hdc , _pos , 50 );
+	const LineMesh* mesh = GET_SINGLE ( ResourceManager )->GetLineMesh ( L"Player" );
+	if ( mesh )
+		mesh->Render ( hdc, _pos );
 }
---
+++ pokemon-clone/pokemon-clone/ResourceManager.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+class ResourceBase;
+class LineMesh;
+
+class ResourceManager
+{
+	DECLARE_SINGLE ( ResourceManager );
+public:
+	~ResourceManager ( );
+
+public:
+	void Init ( );
+	void Clear ( );
+
+	const LineMesh* GetLineMesh ( wstring key );
+
+private:
+	unordered_map<wstring , LineMesh*> _lineMeshes;
+};
+
