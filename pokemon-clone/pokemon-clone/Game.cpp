01840c94b1383ae0fa5f83feecff8305c04cc8a9
5yrotciv <osw9921@gmail.com>
2025-12-18 08:39:51 +0900

    feat: 리소스 매니저

--- pokemon-clone/pokemon-clone/Game.cpp
+++ pokemon-clone/pokemon-clone/Game.cpp
@@ -4,6 +4,7 @@
 #include "TimeManager.h"
 #include "InputManager.h"
 #include "SceneManager.h"
+#include "ResourceManager.h"
 
 Game::Game ( )
 {
@@ -12,6 +13,7 @@ Game::Game ( )
 Game::~Game ( )
 {
 	GET_SINGLE ( SceneManager )->Clear ( );
+	GET_SINGLE ( ResourceManager )->Clear ( );
 
 	_CrtDumpMemoryLeaks ( );
 }
@@ -31,8 +33,9 @@ void Game::Init ( HWND hwnd )
 	GET_SINGLE ( TimeManager  )->Init ( );
 	GET_SINGLE ( InputManager )->Init ( hwnd );
 	GET_SINGLE ( SceneManager )->Init ( );
+	GET_SINGLE ( ResourceManager )->Init ( );
 
-	GET_SINGLE ( SceneManager )->ChangeScene ( SceneType::EditScene );
+	GET_SINGLE ( SceneManager )->ChangeScene ( SceneType::GameScene );
 }
 
 void Game::Update ( )
