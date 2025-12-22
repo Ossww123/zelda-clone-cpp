6dbf294327b5392854bad9258d32837f68bccf5e
5yrotciv <osw9921@gmail.com>
2025-12-22 13:21:15 +0900

    feat: UI 버튼

---
+++ zelda-clone/zelda-clone/TestButton.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "Button.h"
+
+class TestButton : public Button
+{
+	using Spuer = Button;
+public:
+	TestButton ( );
+	virtual ~TestButton ( ) override;
+
+	virtual void BeginPlay ( ) override;
+	virtual void Tick ( ) override;
+	virtual void Render ( HDC hdc ) override;
+};
+
