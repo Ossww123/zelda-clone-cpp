1b8b6b87ce1b59ddf06a78e428e1b1ebcf988b16
5yrotciv <osw9921@gmail.com>
2026-01-02 13:09:34 +0900

    feat: 오브젝트 설

---
+++ zelda-clone/zelda-clone/Creature.h
@@ -0,0 +1,29 @@
+﻿#pragma once
+#include "GameObject.h"
+
+class Creature : public GameObject
+{
+	using Super = GameObject;
+
+public:
+	Creature ( );
+	virtual ~Creature ( ) override;
+
+	virtual void BeginPlay ( ) override;
+	virtual void Tick ( ) override;
+	virtual void Render ( HDC hdc ) override;
+
+private:
+
+	virtual void TickIdle ( ) override {};
+	virtual void TickMove ( ) override {};
+	virtual void TickSkill ( ) override {};
+	virtual void UpdateAnimation ( ) override {};
+
+	void SetStatus ( Status status ) { _status = status; }
+	Status& GetStatus ( ) { return _status; }
+
+protected:
+	Status _status;
+};
+
---
+++ zelda-clone/zelda-clone/Monster.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "Monster.h"
+
+Monster::Monster()
+{
+}
+
+Monster::~Monster()
+{
+}
+
+void Monster::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// TODO
+}
+
+void Monster::Tick()
+{
+	Super::Tick();
+
+	// TODO
+}
+
+void Monster::Render(HDC hdc)
+{
+	Super::Render(hdc);
+
+	// TODO
+}
