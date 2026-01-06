39c08c5d9385be139c62ba87c1eb7c89bd0ea82c
5yrotciv <osw9921@gmail.com>
2026-01-06 13:55:51 +0900

    feat: iocpCore

--- Server/Server/Server.cpp
+++ Server/Server/Server.cpp
@@ -9,119 +9,30 @@
 #include <queue>
 using namespace std;
 #include "ThreadManager.h"
+#include "SocketUtils.h"
+#include "Listener.h"
 
-const int32 BUF_SIZE = 1000;
-
-struct Session
-{
-	SOCKET socket = INVALID_SOCKET;
-	char recvBuffer[BUF_SIZE] = {};
-	int32 recvBytes = 0;
-};
-
-enum IO_TYPE
-{
-	READ,
-	WRITE,
-	ACCEPT,
-	CONNECT,
-};
-
-struct OverlappedEx
-{
-	WSAOVERLAPPED overlapped = {};
-	int32 type = 0;
-	// TODO
-};
-
-void WorkerThreadMain(HANDLE iocpHandle)
-{
-	while (true)
-	{
-		DWORD bytesTransferred = 0;
-		Session* session = nullptr;
-		OverlappedEx* overlappedEx = nullptr;
-
-		BOOL ret = ::GetQueuedCompletionStatus(iocpHandle, &bytesTransferred,
-			(ULONG_PTR*)&session, (LPOVERLAPPED*)&overlappedEx, INFINITE);
-
-		if (ret == false || bytesTransferred == 0)
-		{
-			// 연결 끊김
-			continue;
-		}
-
-		assert(overlappedEx->type == IO_TYPE::READ);
-
-		cout << "Recv Data Len = " << bytesTransferred << endl;
-		cout << "Recv Data IOCP = " << session->recvBuffer << endl;
-
-		WSABUF wsaBuf;
-		wsaBuf.buf = session->recvBuffer;
-		wsaBuf.len = BUF_SIZE;
-
-		DWORD recvLen = 0;
-		DWORD flags = 0;
-		::WSARecv(session->socket, &wsaBuf, 1, &recvLen, &flags, &overlappedEx->overlapped, NULL);
-	}
-}
 
 int main()
 {
 	SocketUtils::Init();
 
-	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
-	if (listenSocket == INVALID_SOCKET)
-		return 0;
+	Listener listener;
+	listener.StartAccept(NetAddress(L"127.0.0.1", 7777));
 
-	SocketUtils::SetReuseAddress(listenSocket, true);
-
-	if (SocketUtils::BindAnyAddress(listenSocket, 7777) == false)
-		return 0;
-
-	if (SocketUtils::Listen(listenSocket) == false)
-		return 0;
-
-	vector<Session*> sessionManager;
-
-	// CP 생성
-	HANDLE iocpHandle = ::CreateIoCompletionPort(INVALID_HANDLE_VALUE, NULL, 0, 0);
-	
-	// WorkerTheads
 	for (int32 i = 0; i < 5; i++)
-		GThreadManager->Launch([=]() {WorkerThreadMain(iocpHandle); });
-
-	// Accept
-	while (true)
 	{
-		SOCKADDR_IN clientAddr;
-		int32 addrLen = sizeof(clientAddr);
-
-		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
-		if (clientSocket == INVALID_SOCKET)
-			return 0;
-
-		Session* session = new Session();
-		session->socket = clientSocket;
-		sessionManager.push_back(session);
-
-		cout << "Client Connected" << endl;
-
-		// 소켓을 CP에 등록
-		::CreateIoCompletionPort((HANDLE)clientSocket, iocpHandle, /*Key*/(ULONG_PTR)session, 0);
-
-		WSABUF wsaBuf;
-		wsaBuf.buf = session->recvBuffer;
-		wsaBuf.len = BUF_SIZE;
-
-		OverlappedEx* overlappedEx = new OverlappedEx();
-		overlappedEx->type = IO_TYPE::READ;
-
-		// ADD_REF
-		DWORD recvLen = 0;
-		DWORD flags = 0;
-		::WSARecv(clientSocket, &wsaBuf, 1, &recvLen, &flags, &overlappedEx->overlapped, NULL);
+		GThreadManager->Launch([=]()
+			{
+				while (true)
+				{
+					GIocpCore.Dispatch();
+				}
+			});
 	}
 
+	GThreadManager->Join();
+
+	// 윈속 종료
 	SocketUtils::Clear();
 }
