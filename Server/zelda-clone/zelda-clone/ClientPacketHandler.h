dfcf974540d3fdcc09ecba1ea28b1dcc1bf970da
5yrotciv <osw9921@gmail.com>
2026-01-09 20:09:47 +0900

    feat: 클라이언트 연동

--- Server/zelda-clone/zelda-clone/ClientPacketHandler.h
+++ Server/zelda-clone/zelda-clone/ClientPacketHandler.h
@@ -1,8 +1,9 @@
-#pragma once
+ï»¿#pragma once
 
 enum
 {
-	S_TEST = 1
+	S_TEST = 1 ,
+	S_EnterGame = 2 ,
 };
 
 class ClientPacketHandler
@@ -10,10 +11,11 @@ class ClientPacketHandler
 public:
 	static void HandlePacket(BYTE* buffer, int32 len);
 
-	// ¹Þ±â
+	// ë°›ê¸°
 	static void Handle_S_TEST(BYTE* buffer, int32 len);
+	static void Handle_S_EnterGame ( BYTE* buffer , int32 len );
 
-	// º¸³»±â
+	// ë³´ë‚´ê¸°
 	template<typename T>
 	static SendBufferRef MakeSendBuffer(T& pkt, uint16 pktId)
 	{
