a6046e9b876aa202877861199632717d15d375d6
5yrotciv <osw9921@gmail.com>
2026-01-09 13:06:02 +0900

    feat: ProfoBuf

--- Server/Server/ServerPacketHandler.h
+++ Server/Server/ServerPacketHandler.h
@@ -16,6 +16,25 @@ class ServerPacketHandler
 public:
 	static void HandlePacket(BYTE* buffer, int32 len);
 
+	// 받기
+
+	// 보내기
 	static SendBufferRef Make_S_TEST(uint64 id, uint32 hp, uint16 attack, vector<BuffData> buffs);
+
+	template<typename T>
+	static SendBufferRef MakeSendBuffer(T& pkt, uint16 pktId)
+	{
+		const uint16 dataSize = static_cast<uint16>(pkt.ByteSizeLong());
+		const uint16 packetSize = dataSize + sizeof(PacketHeader);
+
+		SendBufferRef sendBuffer = make_shared<SendBuffer>(packetSize);
+		PacketHeader* header = reinterpret_cast<PacketHeader*>(sendBuffer->Buffer());
+		header->size = packetSize;
+		header->id = pktId;
+		assert(pkt.SerializeToArray(&header[1], dataSize));
+		sendBuffer->Close(packetSize);
+
+		return sendBuffer;
+	}
 };
 
