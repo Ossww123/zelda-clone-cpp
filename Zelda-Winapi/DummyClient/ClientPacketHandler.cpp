3a13eacd8f28464cc15f0c067d096f82065c10da
5yrotciv <osw9921@gmail.com>
2026-02-13 05:08:27 +0900

    feat: 더미 클라이언트 추가

--- Zelda-Winapi/DummyClient/ClientPacketHandler.cpp
+++ Zelda-Winapi/DummyClient/ClientPacketHandler.cpp
@@ -1,40 +1,133 @@
-#include "pch.h"
+﻿#include "pch.h"
 #include "ClientPacketHandler.h"
 #include "BufferReader.h"
 
+static std::atomic<uint64> GRecvEnterGame = 0;
+static std::atomic<uint64> GRecvMove = 0;
+static std::atomic<uint64> GRecvAttack = 0;
+static std::atomic<uint64> GRecvDamaged = 0;
+static std::atomic<uint64> GRecvTurn = 0;
+
 void ClientPacketHandler::HandlePacket(BYTE* buffer, int32 len)
 {
-	BufferReader br(buffer, len);
+    BufferReader br(buffer, len);
 
-	PacketHeader header;
-	br >> header;
+    PacketHeader header;
+    br >> header;
 
-	switch (header.id)
-	{
-	case S_TEST:
-		Handle_S_TEST(buffer, len);
-		break;
-	}
+    switch (header.id)
+    {
+    case S_TEST:
+        Handle_S_TEST(buffer, len);
+        break;
+    case S_EnterGame:
+        Handle_S_EnterGame(buffer, len);
+        break;
+    case S_Move:
+        Handle_S_Move(buffer, len);
+        break;
+    case S_Attack:
+        Handle_S_Attack(buffer, len);
+        break;
+    case S_Damaged:
+        Handle_S_Damaged(buffer, len);
+        break;
+    case S_Turn:
+        Handle_S_Turn(buffer, len);
+        break;
+    default:
+        break;
+    }
 }
 
 void ClientPacketHandler::Handle_S_TEST(BYTE* buffer, int32 len)
 {
-	PacketHeader* header = (PacketHeader*)buffer;
-	//uint16 id = header->id;
-	uint16 size = header->size;
+    PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
+    uint16 size = header->size;
 
-	Protocol::S_TEST pkt;
-	pkt.ParseFromArray(&header[1], size - sizeof(PacketHeader));
+    Protocol::S_TEST pkt;
+    pkt.ParseFromArray(&header[1], size - sizeof(PacketHeader));
+}
 
-	uint64 id = pkt.id();
-	uint32 hp = pkt.hp();
-	uint16 attack = pkt.attack();
+void ClientPacketHandler::Handle_S_EnterGame(BYTE* buffer, int32 len)
+{
+    PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
+    uint16 size = header->size;
 
-	cout << "ID: " << id << " HP : " << hp << " ATT : " << attack << endl;
+    Protocol::S_EnterGame pkt;
+    if (pkt.ParseFromArray(&header[1], size - sizeof(PacketHeader)))
+        GRecvEnterGame.fetch_add(1);
+}
 
-	for (int32 i = 0; i < pkt.buffs_size(); i++)
-	{
-		const Protocol::BuffData& data = pkt.buffs(i);
-		cout << "BuffInfo : " << data.buffid() << " " << data.remaintime() << endl;
-	}
+void ClientPacketHandler::Handle_S_Move(BYTE* buffer, int32 len)
+{
+    PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
+    uint16 size = header->size;
+
+    Protocol::S_Move pkt;
+    if (pkt.ParseFromArray(&header[1], size - sizeof(PacketHeader)))
+        GRecvMove.fetch_add(1);
+}
+
+void ClientPacketHandler::Handle_S_Attack(BYTE* buffer, int32 len)
+{
+    PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
+    uint16 size = header->size;
+
+    Protocol::S_Attack pkt;
+    if (pkt.ParseFromArray(&header[1], size - sizeof(PacketHeader)))
+        GRecvAttack.fetch_add(1);
+}
+
+void ClientPacketHandler::Handle_S_Damaged(BYTE* buffer, int32 len)
+{
+    PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
+    uint16 size = header->size;
+
+    Protocol::S_Damaged pkt;
+    if (pkt.ParseFromArray(&header[1], size - sizeof(PacketHeader)))
+        GRecvDamaged.fetch_add(1);
+}
+
+void ClientPacketHandler::Handle_S_Turn(BYTE* buffer, int32 len)
+{
+    PacketHeader* header = reinterpret_cast<PacketHeader*>(buffer);
+    uint16 size = header->size;
+
+    Protocol::S_Turn pkt;
+    if (pkt.ParseFromArray(&header[1], size - sizeof(PacketHeader)))
+        GRecvTurn.fetch_add(1);
+}
+
+SendBufferRef ClientPacketHandler::Make_C_Login(const std::string& username)
+{
+    Protocol::C_Login pkt;
+    pkt.set_username(username);
+    return MakeSendBuffer(pkt, C_Login);
+}
+
+SendBufferRef ClientPacketHandler::Make_C_Move(Protocol::DIR_TYPE dir)
+{
+    Protocol::C_Move pkt;
+    pkt.set_dir(dir);
+    return MakeSendBuffer(pkt, C_Move);
+}
+
+SendBufferRef ClientPacketHandler::Make_C_Attack(Protocol::DIR_TYPE dir, Protocol::WEAPON_TYPE weaponType)
+{
+    Protocol::C_Attack pkt;
+    pkt.set_dir(dir);
+    pkt.set_weapontype(weaponType);
+    return MakeSendBuffer(pkt, C_Attack);
+}
+
+DummyClientStats ClientPacketHandler::ConsumeStats()
+{
+    DummyClientStats stats;
+    stats.recvEnterGame = GRecvEnterGame.exchange(0);
+    stats.recvMove = GRecvMove.exchange(0);
+    stats.recvAttack = GRecvAttack.exchange(0);
+    stats.recvDamaged = GRecvDamaged.exchange(0);
+    stats.recvTurn = GRecvTurn.exchange(0);
+    return stats;
 }
--- Zelda-Winapi/DummyClient/ClientPacketHandler.h
+++ Zelda-Winapi/DummyClient/ClientPacketHandler.h
@@ -1,33 +1,84 @@
-#pragma once
+ï»¿#pragma once
 
 enum
 {
-	S_TEST = 1
+    C_Move = 101,
+    C_Attack = 102,
+    C_ChangeMap = 103,
+    C_Turn = 104,
+    C_EquipItem = 105,
+    C_UnequipItem = 106,
+    C_UseItem = 107,
+    C_PartyInvite = 108,
+    C_PartyAnswer = 109,
+    C_PartyLeave = 110,
+    C_Login = 111,
+
+    S_TEST = 201,
+    S_EnterGame = 202,
+    S_MyPlayer = 203,
+    S_AddObject = 204,
+    S_RemoveObject = 205,
+    S_Move = 206,
+    S_Attack = 207,
+    S_Damaged = 208,
+    S_ChangeMap = 209,
+    S_GainExp = 210,
+    S_LevelUp = 211,
+    S_Turn = 212,
+    S_InventoryData = 213,
+    S_AddItem = 214,
+    S_EquipItem = 215,
+    S_UnequipItem = 216,
+    S_UseItem = 217,
+    S_PartyInvite = 218,
+    S_PartyUpdate = 219,
+    S_PartyLeave = 220,
+};
+
+struct DummyClientStats
+{
+    uint64 recvEnterGame = 0;
+    uint64 recvMove = 0;
+    uint64 recvAttack = 0;
+    uint64 recvDamaged = 0;
+    uint64 recvTurn = 0;
 };
 
 class ClientPacketHandler
 {
 public:
-	static void HandlePacket(BYTE* buffer, int32 len);
+    static void HandlePacket(BYTE* buffer, int32 len);
 
-	// ¹Þ±â
-	static void Handle_S_TEST(BYTE* buffer, int32 len);
+    // receive
+    static void Handle_S_TEST(BYTE* buffer, int32 len);
+    static void Handle_S_EnterGame(BYTE* buffer, int32 len);
+    static void Handle_S_Move(BYTE* buffer, int32 len);
+    static void Handle_S_Attack(BYTE* buffer, int32 len);
+    static void Handle_S_Damaged(BYTE* buffer, int32 len);
+    static void Handle_S_Turn(BYTE* buffer, int32 len);
 
-	// º¸³»±â
-	template<typename T>
-	static SendBufferRef MakeSendBuffer(T& pkt, uint16 pktId)
-	{
-		const uint16 dataSize = static_cast<uint16>(pkt.ByteSizeLong());
-		const uint16 packetSize = dataSize + sizeof(PacketHeader);
+    // send
+    static SendBufferRef Make_C_Login(const std::string& username);
+    static SendBufferRef Make_C_Move(Protocol::DIR_TYPE dir);
+    static SendBufferRef Make_C_Attack(Protocol::DIR_TYPE dir, Protocol::WEAPON_TYPE weaponType);
 
-		SendBufferRef sendBuffer = make_shared<SendBuffer>(packetSize);
-		PacketHeader* header = reinterpret_cast<PacketHeader*>(sendBuffer->Buffer());
-		header->size = packetSize;
-		header->id = pktId;
-		assert(pkt.SerializeToArray(&header[1], dataSize));
-		sendBuffer->Close(packetSize);
+    // stats
+    static DummyClientStats ConsumeStats();
 
-		return sendBuffer;
-	}
+    template<typename T>
+    static SendBufferRef MakeSendBuffer(T& pkt, uint16 pktId)
+    {
+        const uint16 dataSize = static_cast<uint16>(pkt.ByteSizeLong());
+        const uint16 packetSize = dataSize + sizeof(PacketHeader);
+
+        SendBufferRef sendBuffer = make_shared<SendBuffer>(packetSize);
+        PacketHeader* header = reinterpret_cast<PacketHeader*>(sendBuffer->Buffer());
+        header->size = packetSize;
+        header->id = pktId;
+        assert(pkt.SerializeToArray(&header[1], dataSize));
+        sendBuffer->Close(packetSize);
+
+        return sendBuffer;
+    }
 };
-
